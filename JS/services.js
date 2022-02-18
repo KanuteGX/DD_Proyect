da946a02865fe4f6f9382fe6bd8293bdbae300a9
KanuteGX <pimentel2002d@gmail.com>
2022-02-17 23:11:56 -0400

    creation complet page 0.6

--- JS/services.js
+++ JS/services.js
@@ -3,36 +3,22 @@ window.addEventListener('scroll', () => {
 });
 
 // Element scroll
-const cardService0 = document.getElementById('cardService-0');
-const cardService1 = document.getElementById('cardService-1');
-const cardService2 = document.getElementById('cardService-2');
-const cardService3 = document.getElementById('cardService-3');
-const cardService4 = document.getElementById('cardService-4');
+const containerServices = document.getElementById('containerServices');
+
+const containerServicesArray = Array.from(containerServices.children);
+
 const containerCardTwo = document.getElementById('containerCardTwo');
 const cardTwo = document.getElementById('cardTwo');
 
 const elementAnimationScroll = () => {
   let scrollTop = document.firstElementChild.scrollTop;
-  let cardServiceTop0 = cardService0.offsetTop;
-  let cardServiceTop1 = cardService1.offsetTop;
-  let cardServiceTop2 = cardService2.offsetTop;
-  let cardServiceTop3 = cardService3.offsetTop;
-  let cardServiceTop4 = cardService4.offsetTop;
   let viewportHeight = document.firstElementChild.clientHeight;
 
-  if (scrollTop > cardServiceTop0 - viewportHeight / 1.3)
-    cardService0.classList.add('card-service__scroll');
-
-  if (scrollTop > cardServiceTop1 - viewportHeight / 1.3)
-    cardService1.classList.add('card-service__scroll');
-
-  if (scrollTop > cardServiceTop2 - viewportHeight / 1.3)
-    cardService2.classList.add('card-service__scroll');
-
-  if (scrollTop > cardServiceTop3 - viewportHeight / 1.3)
-    cardService3.classList.add('card-service__scroll');
-
-  if (scrollTop > cardServiceTop4 - viewportHeight / 1.3)
-    cardService4.classList.add('card-service__scroll');
+  containerServicesArray.forEach((card, index) => {
+    if (index != 0) {
+      let cardTop = card.offsetTop;
+      if (scrollTop > cardTop - viewportHeight / 1.3)
+        card.classList.add('card-service__scroll');
+    }
+  });
 };
-// || scrollTop > cardService1 - viewportHeight / 1.3 || scrollTop > cardService2 - viewportHeight / 1.3 || scrollTop > cardService3 - viewportHeight / 1.3 || scrollTop > cardService4 - viewportHeight / 1.3
